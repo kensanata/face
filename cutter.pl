01ea6af3ff4ff5d03d0a220f36a0970b1ab2c7a0
Alex Schroeder <alex@gnu.org>
2015-09-24 09:03:00 +0200

    cutter.pl: allow the provision of just one element

--- cutter.pl
+++ cutter.pl
@@ -11,8 +11,9 @@ $image = GD::Image->newFromJpeg($file) if $file =~ /\.jpe?g$/i;
 die "Cannot read image from $file: $!" unless $image;
 
 # assuming 300dpi, looking at 1.5in x 2.0in images
+my $type;
 for my $y (0..4) {
-  my $type = shift || die "Must provide five types ('eyes', 'nose', etc.)\n";
+  $type = shift || $type || die "Must provide five types ('eyes', 'nose', etc.)\n";
   for my $x (0..4) {
     my $element = new GD::Image(450,600);
     $element->copy($image, 0, 0, 450 * $x, 600 * $y, 450, 600);
