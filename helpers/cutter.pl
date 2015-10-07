92048af102650a90886f1497c5d894c8101bfcfe
Alex Schroeder <alex@gnu.org>
2015-10-07 19:59:20 +0200

    cutter.pl limits itself to source image size

--- helpers/cutter.pl
+++ helpers/cutter.pl
@@ -12,18 +12,26 @@ $image = GD::Image->newFromJpeg($file) if $file =~ /\.jpe?g$/i;
 die "Cannot read image from $file: $!" unless $image;
 
 # assuming 300dpi, looking at 1.5in x 2.0in images
-my $type;
-for my $y (0..4) {
-  $type = shift || $type || die "Must provide five types ('eyes', 'nose', etc.)\n";
-  for my $x (0..4) {
-    my $element = new GD::Image(450,600);
-    $element->copy($image, 0, 0, 450 * $x, 600 * $y, 450, 600);
-    my $c = 1;
-    $c++ while -f "elements/$artist/${type}_$c.png";
-    open (my $fh, '>', "elements/$artist/${type}_$c.png")
-	or die "Cannot write elements/$artist/${type}_$c.png: $!";
-    binmode $fh;
-    print $fh $element->png;
-    close $fh;
+
+sub process {
+  my @types = @_;
+  my $type;
+  for my $y (0..4) {
+    last if 600 * ($y + 1) > $image->height;
+    $type = shift(@types) || $type || die "You must provide at least one type ('eyes', 'nose', etc.)\n";
+    for my $x (0..4) {
+      last if 450 * ($x + 1) > $image->width;
+      my $element = new GD::Image(450,600);
+      $element->copy($image, 0, 0, 450 * $x, 600 * $y, 450, 600);
+      my $c = 1;
+      $c++ while -f "elements/$artist/${type}_$c.png";
+      open (my $fh, '>', "elements/$artist/${type}_$c.png")
+	  or die "Cannot write elements/$artist/${type}_$c.png: $!";
+      binmode $fh;
+      print $fh $element->png;
+      close $fh;
+    }
   }
 }
+
+process(@ARGV);
